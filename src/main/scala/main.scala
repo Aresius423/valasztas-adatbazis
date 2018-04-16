23f0fc9e8aa50bb0fd8eab2f0e2d99ecd9f621a4
Markus Janos Daniel <aresius423@gmail.com>
2018-04-16 16:44:02 +0200

    File read failure handling, working database

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,17 +1,33 @@
 // Import the Slick interface for H2:
 import slick.jdbc.H2Profile.api._
-import scala.concurrent.ExecutionContext.Implicits.global
+import helpers.SqlHelpers
 import helpers.SqlHelperImplicits._
+import scala.util.{Success,Failure}
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
 
 import schema.Tables._
 
 object Main extends App {
   val db = Database.forConfig("election_analysis")
-  val files = Seq(
-	"schema.sql"
-  )
+  val importSchemaFuture = db.execFile("schema.sql")
+	
+  val importFuture = importSchemaFuture.flatMap(_ => db.execFile(SqlHelpers.sqlFiles))
+  Await.result(importFuture, 30.seconds)
   
-  db.execFile(files)
+  importSchemaFuture.onComplete{
+	case Success(i) => {
+		importFuture.onComplete{
+			case Success(i) => println("Database import successful")
+			case Failure(f) => println("Database import unsuccessful")
+		}
+	}
+	case Failure(f) => println("Schema import unsuccessful")
+  }
+  
+  println(Await.result(db.run(TableQuery[Verzio].result), 2.seconds))
   
   println("Hello world!")
 }
--- src/main/scala/sqlFromFile.scala
+++ src/main/scala/sqlFromFile.scala
@@ -1,21 +1,39 @@
 package helpers
 
-import java.io.File
 import scala.io.Source
 import slick.jdbc.H2Profile.api._
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Try,Success,Failure}
+import java.io.File
+
+object SqlHelpers{
+	def sqlFiles:Seq[String] = {
+		val sqls = new java.io.File("./sql").listFiles.filter(_.getName.endsWith(".sql"))
+		sqls.map(file => "sql/" + file.getName).reverse
+	}
+
+}
 
 object SqlHelperImplicits{
 	implicit class SqlFromFile(db:Database){
 		def execFile(path:Seq[String]):Future[Any] = {
 			val futures = path.map(file => {
-				val source = Source.fromFile(file)("UTF-8")
-				val cmd:String = source.getLines.mkString
-				db.run(sql"#$cmd".as[Int]) //not safe for user input
+				Try(Source.fromFile(file)("UTF-8")) match {
+					case Success(src) => {
+						val cmd:String = src.getLines.mkString("\r\n")
+						db.run(sql"#$cmd".as[Int]) //not safe for user input
+					}
+					case Failure(f) => {
+						println("fuck")
+						Future.failed(f)
+					}
+				}
 			})
 			
 			Future.sequence(futures)
 		}
+		
+		def execFile(path:String):Future[Any] = execFile(Seq(path))
 	}
 }
